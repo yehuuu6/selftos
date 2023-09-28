cb2e39fe078b3a37572b82858c08027bfc740e88
sscorpi <therenaydin@gmail.com>
2023-09-28 15:39:36 +0300

    Changed run to execute in command methods

--- utils/commands.py
+++ utils/commands.py
@@ -26,7 +26,7 @@ def broadcast(users: List[SelftosNetwork.User], msg: str, msg_source: str = FILE
         package = SelftosNetwork.Package(type = "SFSMessage", content = msg, source = msg_source)
         SelftosNetwork.send_package(package, user.client)
 
-def run_list(users: List[SelftosNetwork.User], args: List[str]) -> None:
+def execute_list(users: List[SelftosNetwork.User], args: List[str]) -> None:
     if len(args) == 0:
         SelftosUtils.printf("<SERVER> Usage: list <request>")
         return
@@ -40,14 +40,14 @@ def run_list(users: List[SelftosNetwork.User], args: List[str]) -> None:
     else:
         SelftosUtils.printf("<[red]ERROR[/red]> [red]You can't list that.[/red]")
 
-def run_say(users: List[SelftosNetwork.User], args: List[str]) -> None:
+def execute_say(users: List[SelftosNetwork.User], args: List[str]) -> None:
     if len(args) == 0:
         SelftosUtils.printf("<SERVER> Usage: say <message>")
         return
     msg = " ".join(args)
     broadcast(users, msg, f"[red3]Admin[/red3]", render_on_server=True)
 
-def run_who(users: List[SelftosNetwork.User], args: List[str]) -> None:
+def execute_who(users: List[SelftosNetwork.User], args: List[str]) -> None:
     if len(args) == 0:
             SelftosUtils.printf("<SERVER> Usage: who <user_name>")
     else:
@@ -59,7 +59,7 @@ def run_who(users: List[SelftosNetwork.User], args: List[str]) -> None:
         else:
             SelftosUtils.printf("<[red]ERROR[/red]> [red]User not found.[/red]")
 
-def run_kick(users: List[SelftosNetwork.User], args: List[str]) -> None:
+def execute_kick(users: List[SelftosNetwork.User], args: List[str]) -> None:
     if len(args) == 0:
             SelftosUtils.printf("<SERVER> Usage: kick <user_name>")
     else:
@@ -74,7 +74,7 @@ def run_kick(users: List[SelftosNetwork.User], args: List[str]) -> None:
         else:
             SelftosUtils.printf("<[red]ERROR[/red]> [red]User not found.[/red]")
 
-def run_mute(users: List[SelftosNetwork.User], args: List[str]) -> None:
+def execute_mute(users: List[SelftosNetwork.User], args: List[str]) -> None:
     if len(args) == 0:
             SelftosUtils.printf("<SERVER> Usage: mute <user_name>")
     else:
@@ -89,7 +89,7 @@ def run_mute(users: List[SelftosNetwork.User], args: List[str]) -> None:
         else:
             SelftosUtils.printf("<[red]ERROR[/red]> [red]User not found.[/red]")
 
-def run_unmute(users: List[SelftosNetwork.User], args: List[str]) -> None:
+def execute_unmute(users: List[SelftosNetwork.User], args: List[str]) -> None:
     if len(args) == 0:
             SelftosUtils.printf("<SERVER> Usage: unmute <user_name>")
     else:
@@ -104,7 +104,7 @@ def run_unmute(users: List[SelftosNetwork.User], args: List[str]) -> None:
         else:
             SelftosUtils.printf("<[red]ERROR[/red]> [red]User not found.[/red]")
 
-def run_admin(users: List[SelftosNetwork.User], args: List[str]) -> None:
+def execute_admin(users: List[SelftosNetwork.User], args: List[str]) -> None:
     if len(args) == 0:
             SelftosUtils.printf("<SERVER> Usage: admin <user_name>")
     else:
@@ -119,7 +119,7 @@ def run_admin(users: List[SelftosNetwork.User], args: List[str]) -> None:
         else:
             SelftosUtils.printf("<[red]ERROR[/red]> [red]User not found.[/red]")
 
-def run_unadmin(users: List[SelftosNetwork.User], args: List[str]) -> None:
+def execute_unadmin(users: List[SelftosNetwork.User], args: List[str]) -> None:
     if len(args) == 0:
         SelftosUtils.printf("<SERVER> Usage: unadmin <user_name>")
     else:
@@ -134,7 +134,7 @@ def run_unadmin(users: List[SelftosNetwork.User], args: List[str]) -> None:
         else:
             SelftosUtils.printf("<[red]ERROR[/red]> [red]User not found.[/red]")
 
-def run_ban(users: List[SelftosNetwork.User], args: List[str]) -> None:
+def execute_ban(users: List[SelftosNetwork.User], args: List[str]) -> None:
     if len(args) == 0:
             SelftosUtils.printf("<SERVER> Usage: ban <user_name>")
     else:
@@ -149,34 +149,34 @@ def run_ban(users: List[SelftosNetwork.User], args: List[str]) -> None:
         else:
             SelftosUtils.printf("<[red]ERROR[/red]> [red]User not found.[/red]")
 
-def run_clear(users: List[SelftosNetwork.User], args: List[str]) -> None:
+def execute_clear(users: List[SelftosNetwork.User], args: List[str]) -> None:
     SelftosUtils.clear_console()
 
-def run_help(users: List[SelftosNetwork.User], args: List[str]) -> None:
-    SelftosUtils.printf("<SERVER> List of commands ~ [blue]arguments marked with * are required[/blue]");
-    SelftosUtils.printf("[magenta]~ list[/magenta] " + escape("[items*]") + " - [yellow]Lists the specified list.[/yellow]");
-    SelftosUtils.printf(f"[magenta]~ say[/magenta] " + escape("[message*]") + " - [yellow]Broadcasts a message to all the users in the room as [red3]Admin[/red3].[/yellow]");
-    SelftosUtils.printf(f"[magenta]~ kick[/magenta] " + escape("[name*]") + " - [yellow]Kicks the specified user from the room.[/yellow]");
-    SelftosUtils.printf(f"[magenta]~ mute[/magenta] " + escape("[name*]") + " - [yellow]Mutes the specified user.[/yellow]");
-    SelftosUtils.printf(f"[magenta]~ unmute[/magenta] " + escape("[name*]") + " - [yellow]Unmutes the specified user.[/yellow]");
-    SelftosUtils.printf(f"[magenta]~ admin[/magenta] " + escape("[name*]") + " - [yellow]Grants admin privileges to the specified user.[/yellow]");
-    SelftosUtils.printf(f"[magenta]~ unadmin[/magenta] " + escape("[name*]") + " - [yellow]Revokes admin privileges from the specified user.[/yellow]");
-    SelftosUtils.printf(f"[magenta]~ ban[/magenta] " + escape("[name*]") + " - [yellow]Bans the specified user from the room.[/yellow]");
-    SelftosUtils.printf(f"[magenta]~ who[/magenta] " + escape("[name*]") + " - [yellow]Shows information about the specified user.[/yellow]");
-    SelftosUtils.printf(f"[magenta]~ clear[/magenta] - [yellow]Clears the console.[/yellow]");
-    SelftosUtils.printf(f"[magenta]~ shutdown[/magenta] " + escape("[reason]") + " - [yellow]Closes the server.[/yellow]");
+def execute_help(users: List[SelftosNetwork.User], args: List[str]) -> None:
+    SelftosUtils.printf("<SERVER> List of commands | [blue]Arguments marked with * are required[/blue]");
+    SelftosUtils.printf(f"[magenta]~ list[/magenta] [yellow]{escape('[items*]')}[/yellow] - Prints out a list of the specified items.");
+    SelftosUtils.printf(f"[magenta]~ say[/magenta] [yellow]{escape('[message*]')}[/yellow] - Broadcasts a message to all the users in the room as admin.");
+    SelftosUtils.printf(f"[magenta]~ kick[/magenta] [yellow]{escape('[user*]')}[/yellow] - Kicks the specified user from the room.");
+    SelftosUtils.printf(f"[magenta]~ mute[/magenta] [yellow]{escape('[user*]')}[/yellow] - Mutes the specified user.");
+    SelftosUtils.printf(f"[magenta]~ unmute[/magenta] [yellow]{escape('[user*]')}[/yellow] - Unmutes the specified user.");
+    SelftosUtils.printf(f"[magenta]~ admin[/magenta] [yellow]{escape('[user*]')}[/yellow] - Grants admin privileges to the specified user.");
+    SelftosUtils.printf(f"[magenta]~ unadmin[/magenta] [yellow]{escape('[user*]')}[/yellow] - Revokes admin privileges from the specified user.");
+    SelftosUtils.printf(f"[magenta]~ ban[/magenta] [yellow]{escape('[user*]')}[/yellow] - Bans the specified user from the room.");
+    SelftosUtils.printf(f"[magenta]~ who[/magenta] [yellow]{escape('[user*]')}[/yellow] - Shows information about the specified user.");
+    SelftosUtils.printf(f"[magenta]~ clear[/magenta] - [yellow]Clears the console.");
+    SelftosUtils.printf(f"[magenta]~ shutdown[/magenta] [yellow]{escape('[reason]')}[/yellow] - Closes the server and broadcasts the reason to all the users in the room.");
 
 COMMANDS = {
     "broadcast": broadcast,
-    "list": run_list,
-    "say": run_say,
-    "who": run_who,
-    "kick": run_kick,
-    "mute": run_mute,
-    "unmute": run_unmute,
-    "admin": run_admin,
-    "unadmin": run_unadmin,
-    "ban": run_ban,
-    "clear": run_clear,
-    "help": run_help
+    "list": execute_list,
+    "say": execute_say,
+    "who": execute_who,
+    "kick": execute_kick,
+    "mute": execute_mute,
+    "unmute": execute_unmute,
+    "admin": execute_admin,
+    "unadmin": execute_unadmin,
+    "ban": execute_ban,
+    "clear": execute_clear,
+    "help": execute_help
 }
