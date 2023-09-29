0a6d0e04ddf82e2878a9d9a992064bd5e446bc55
sscorpi <therenaydin@gmail.com>
2023-09-29 12:56:30 +0300

    Added current time to console output

--- utils/functions.py
+++ utils/functions.py
@@ -9,6 +9,7 @@ from typing import List
 
 import classes.SelftosNetwork as SelftosNetwork
 import socket
+import time
 
 output_buffer = StringIO()
 console = Console(file=output_buffer, color_system="truecolor")
@@ -19,13 +20,19 @@ def clear_console() -> None:
     """
     print("\033[H\033[J", end='')  # ANSI escape sequence to clear the console
 
+def get_current_time() -> str:
+    """
+    Returns the current time in HH:MM format.
+    """
+    return time.strftime("%H:%M:%S", time.localtime(time.time()))
+
 def printf(msg: str) -> None:
     """
     Prints output to the server console.
     """
     output_buffer.seek(0)  # Reset the buffer to the beginning
     output_buffer.truncate(0)  # Clear the buffer content
-    console.print(msg)
+    console.print(f"{get_current_time()} {msg}")
     print_formatted_text(ANSI(output_buffer.getvalue()), end='')
 
 def show_room_config(config: dict) -> None:
