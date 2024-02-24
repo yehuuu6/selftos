f7b6648ff11eed16041fedfa61612cc1642ff375
sscorpi <therenaydin@gmail.com>
2024-02-25 00:05:12 +0300

    Update import path

--- utils/functions.py
+++ utils/functions.py
@@ -7,7 +7,7 @@ from rich.console import Console
 from prompt_toolkit import ANSI, print_formatted_text
 from typing import List
 
-import classes.network as SelftosNetwork
+import library.network as SelftosNetwork
 import socket
 import time
 
@@ -51,22 +51,6 @@ def printc(messages: List[str], executer: SelftosNetwork.User | None) -> None:
     else:
         printf(output)
 
-def show_room_config(config: dict) -> None:
-    printf(f"<CONSOLE> Host is set to [bold yellow]{config['host']}[/bold yellow].")
-    printf(f"<CONSOLE> Port is set to [bold yellow]{config['port']}[/bold yellow].")
-    printf(f"<CONSOLE> ID is set to [bold yellow]{config['id']}[/bold yellow].")
-    printf(f"<CONSOLE> Name is set to [bold yellow]{config['name']}[/bold yellow].")
-    printf(f"<CONSOLE> Description is set to [bold yellow]{config['description']}[/bold yellow].")
-    printf(f"<CONSOLE> Maximum number of users is set to [bold yellow]{config['maxUsers']}[/bold yellow].")
-    printf(f"<CONSOLE> Private is set to [bold yellow]{config['private']}[/bold yellow].")
-    printf(f"<CONSOLE> Owner is set to [bold yellow]{config['owner']['name']}[/bold yellow].")
-    printf(f"<CONSOLE> Show muted messages is set to [bold yellow]{config['show_muted_messages']}[/bold yellow].")
-    printf(f"<CONSOLE> Show executed commands is set to [bold yellow]{config['show_executed_commands']}[/bold yellow].")
-    printf(f"<CONSOLE> Message logging is set to [bold yellow]{config['message_logging']}[/bold yellow].")
-    printf(f"<CONSOLE> Loading roles...")
-    printf(f"<CONSOLE> Default role is set to [bold yellow]{config['default_role']}[/bold yellow].")
-    for role in config['roles']:
-        printf(f"<CONSOLE> [bold yellow]{role['name']}[/bold yellow] role is ready!")
 def get_user_by_socket(sock: socket.socket, users_list: List[SelftosNetwork.User]) -> SelftosNetwork.User | None:
     for user in users_list:
         if user.client == sock:
