69a473857132e26c8a28da5bc632b1803fe982ac
sscorpi <therenaydin@gmail.com>
2024-02-25 20:12:47 +0300

    Update plugin structure

--- dev-tools/plugins/ExamplePlugin.py
+++ dev-tools/plugins/ExamplePlugin.py
@@ -7,13 +7,19 @@ import utils.functions as SelftosUtils
 import socket
 
 class ExamplePlugin:
-    def __init__(self):
+    def __init__(self, online_users: List[SelftosNetwork.User] = []):
         self.name = "Example Plugin"
         self.version = "1.0.0"
         self.description = "An example plugin for the Selftos server"
         self.author = "yehuuu6"
         self.prefix = "<[cyan]ExamplePlugin[/cyan]>"
-        self.online_users: List[SelftosNetwork.User] = []
+        self.online_users = online_users
+
+    def get_user_by_sock(self, target: SelftosNetwork.User) -> SelftosNetwork.User | None:
+        for user in self.online_users:
+            if user.client == target.client:
+                return user
+        return None
 
     def broadcast(self, message: str, render_on_server: bool = False, exclude: SelftosNetwork.User | None = None):
         message = f"{self.prefix} {message}"  # Add the prefix to the message
@@ -28,20 +34,22 @@ class ExamplePlugin:
             except:
                 SelftosUtils.printf(f"<{self.prefix}> Failed to send message to [cyan]{user.name}[/cyan]")
 
-    def on_package_received(self, client_sock: socket.socket, package: SelftosNetwork.Package):
-        pass
+    def on_package_received(self, client_sock: socket.socket, package: SelftosNetwork.Package) -> bool:
+        return True
 
-    def on_message_received(self, user: SelftosNetwork.User, message: str):
-        pass
+    def on_message_received(self, user: SelftosNetwork.User, message: str) -> bool:
+        return True
 
-    def on_user_joined(self, user: SelftosNetwork.User):
+    def on_user_joined(self, user: SelftosNetwork.User) -> None:
         self.online_users.append(user)
 
-    def on_user_left(self, user: SelftosNetwork.User):
-        self.online_users.remove(user)
+    def on_user_left(self, user: SelftosNetwork.User) -> None:
+        target = self.get_user_by_sock(user)
+        if target is not None:
+            self.online_users.remove(target)
 
-    def on_command_executed(self, user: SelftosNetwork.User, command: str, args: List[str]):
-        pass
+    def on_command_executed(self, user: SelftosNetwork.User, command: str, args: List[str]) -> bool:
+        return True
 
 # Entry point for initializing the module
 def PyInit_ExamplePlugin():
